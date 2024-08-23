0da128af075286a69e28a29ac8c8d816326ff8d9
unknown <sharon@basovich.com>
2024-08-23 01:47:59 -0400

    started loggin setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,79 @@
 #include "main.h"
+#include "json.hpp"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 #include "lemlib/chassis/trackingWheel.hpp"
+#include "logging.hpp"
 #include <iostream>
 #include <sys/_intsup.h>
-#include "json.hpp"
-#include "logging.hpp"
 
-pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::MotorGroup
-    left_motors({-1, -2, -3},
-                pros::MotorGearset::blue); // left motors on ports 1, 2, 3
+    left_motors({9, 10, 20},
+                pros::MotorGearset::blue); // left motors on ports 9, 10, 20 with 600 rpm cart
 pros::MotorGroup
-    right_motors({4, 5, 6},
-                 pros::MotorGearset::blue); // right motors on ports 4, 5, 6
-pros::Imu imu(10);
-pros::adi::Encoder adi_encoder('G', 'H');
+    right_motors({-8, -18, -19},
+                 pros::MotorGearset::blue); // right motors on ports 8, 18, 19 with 600 rpm cart
+
+// create an imu on port 17
+pros::Imu imu(17);
+
+// create an optical shaft encoder connected to ports 'A' and 'B'
+pros::adi::Encoder adi_encoder('A', 'B');
+
+// create a v5 rotation sensor on port 7
+pros::Rotation rotation_sensor(7);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motors,               // left motor group
                               &right_motors,              // right motor group
-                              10,                         // 10 inch track width
-                              lemlib::Omniwheel::NEW_275, // using new 4" omnis
-                              450, // drivetrain rpm is 360
+                              11.5,                         // 11.5 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 2.75" omnis
+                              600, // drivetrain rpm is 600
                               2    // horizontal drift is 2 (for now)
 );
-lemlib::TrackingWheel vertical_tracking_wheel(&adi_encoder,
-                                              lemlib::Omniwheel::NEW_275, 0);
+
+lemlib::TrackingWheel horizontal_tracking_wheel(&adi_encoder, lemlib::Omniwheel::NEW_275, -6.5);
+
+lemlib::TrackingWheel vertical_tracking_wheel(&rotation_sensor, lemlib::Omniwheel::NEW_275, 0);
+
 lemlib::OdomSensors sensors(
     &vertical_tracking_wheel, // vertical tracking wheel 1, set to null
-    nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
-    nullptr, // horizontal tracking wheel 1
+    nullptr, // vertical tracking wheel 2, set to nullptr as we have none
+    &horizontal_tracking_wheel, // horizontal tracking wheel 1
     nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a
              // second one
     &imu     // inertial sensor
 );
 
 // lateral PID controller
-lemlib::ControllerSettings
-    lateral_controller(10,  // proportional gain (kP)
-                       0,   // integral gain (kI)
-                       3,   // derivative gain (kD)
-                       3,   // anti windup
-                       1,   // small error range, in inches
-                       100, // small error range timeout, in milliseconds
-                       3,   // large error range, in inches
-                       500, // large error range timeout, in milliseconds
-                       20   // maximum acceleration (slew)
-    );
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
 
 // angular PID controller
-lemlib::ControllerSettings
-    angular_controller(3,   // proportional gain (kP)
-                       0,   // integral gain (kI)
-                       17,  // derivative gain (kD)
-                       3,   // anti windup
-                       1,   // small error range, in degrees
-                       100, // small error range timeout, in milliseconds
-                       3,   // large error range, in degrees
-                       500, // large error range timeout, in milliseconds
-                       0    // maximum acceleration (slew)
-    );
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
 
 lemlib::ExpoDriveCurve
     steer_curve(3,    // joystick deadband out of 127
@@ -72,20 +86,13 @@ lemlib::ExpoDriveCurve
                    10,   // minimum output where drivetrain will move out of 127
                    1.019 // expo curve gain
     );
-
+/*
 lemlib::Chassis chassis(drivetrain,         // drivetrain settings
                         lateral_controller, // lateral PID settings
                         angular_controller, // angular PID settings
                         sensors,            // odometry sensors
                         &throttle_curve, &steer_curve);
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-
+*/
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -94,18 +101,18 @@ lemlib::Chassis chassis(drivetrain,         // drivetrain settings
  */
 void initialize() {
   pros::lcd::initialize(); // initialize brain screen
-  chassis.calibrate();     // calibrate sensors
-  // print position to brain screen
-  pros::Task screen_task([&]() {
-    while (true) {
-      // print robot location to the brain screen
-      pros::lcd::print(0, "X: %f", chassis.getPose().x);         // x
-      pros::lcd::print(1, "Y: %f", chassis.getPose().y);         // y
-      pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-      // delay to save resources
-      pros::delay(20);
-    }
-  });
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 /**
@@ -152,19 +159,19 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
 void opcontrol() {
-  chassis.setPose(0, 0, 0);
+    // loop forever
     while (true) {
-		lemlib::Pose pose = chassis.getPose(true);
+        // get left y and right x positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
-		
+        // move the robot
+        chassis.arcade(leftY, rightX);
 
-        Odometry payload{std::round(pose.x / 100) * 100,
-						std::round(pose.y / 100) * 100,
-						std::round(pose.theta / 100) * 100};
-        Message msg{"odometry", payload};
-        std::cout << static_cast<json>(msg) << std::flush;
-
-        pros::delay(20); 
+        // delay to save resources
+        pros::delay(25);
     }
 }
