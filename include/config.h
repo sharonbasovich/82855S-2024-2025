f4f376fb125f2ece6947eaede825d4853701d542
unknown <sharon@basovich.com>
2024-10-31 15:58:23 -0400

    a

--- include/config.h
+++ include/config.h
@@ -5,12 +5,13 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 
-#define LEFT_FRONT_MOTOR_PORT 20
-#define LEFT_MIDDLE_MOTOR_PORT 19
-#define LEFT_BACK_MOTOR_PORT 8
-#define RIGHT_FRONT_MOTOR_PORT -4
-#define RIGHT_MIDDLE_MOTOR_PORT 20
-#define RIGHT_BACK_MOTOR_PORT -20
+#define LEFT_FRONT_MOTOR_PORT -1
+#define LEFT_MIDDLE_MOTOR_PORT -12
+#define LEFT_BACK_MOTOR_PORT -6
+#define RIGHT_FRONT_MOTOR_PORT 18
+#define RIGHT_MIDDLE_MOTOR_PORT 19
+#define RIGHT_BACK_MOTOR_PORT 20
+#define INTAKE_MOTOR 10
 
 #define DRIVE_GEARSET pros::E_MOTOR_GEARSET_06
 #define WHEEL_DIAMETER 3.25
@@ -27,3 +28,4 @@ inline pros::MotorGroup left_mg({LEFT_FRONT_MOTOR_PORT,LEFT_MIDDLE_MOTOR_PORT,LE
 inline pros::MotorGroup right_mg({RIGHT_FRONT_MOTOR_PORT,RIGHT_MIDDLE_MOTOR_PORT, RIGHT_BACK_MOTOR_PORT},pros::MotorGearset::blue);
 
 inline pros::Controller master(pros::E_CONTROLLER_MASTER);
+inline pros::Motor intake_motor(INTAKE_MOTOR);
