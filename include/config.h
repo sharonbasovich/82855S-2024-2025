9f775eee27a2a93e9b75d976d73e26e2b4b0e929
kerry <kerry4sports@gmail.com>
2024-11-01 15:08:08 -0400

    Mogo mech and doinker

--- include/config.h
+++ include/config.h
@@ -12,6 +12,9 @@
 #define RIGHT_MIDDLE_MOTOR_PORT 19
 #define RIGHT_BACK_MOTOR_PORT 20
 #define INTAKE_MOTOR 10
+#define MogoPistonPort1 'A'
+#define MogoPistonPort2 'B'
+#define doinker 'C'
 
 #define DRIVE_GEARSET pros::E_MOTOR_GEARSET_06
 #define WHEEL_DIAMETER 3.25
@@ -30,3 +33,6 @@ inline pros::MotorGroup right_mg({RIGHT_FRONT_MOTOR_PORT,RIGHT_MIDDLE_MOTOR_PORT
 
 inline pros::Controller master(pros::E_CONTROLLER_MASTER);
 inline pros::Motor intake_motor(INTAKE_MOTOR);
+inline pros::adi::Pneumatics MogoPiston1(MogoPistonPort1);
+inline pros::adi::Pneumatics MogoPiston2(MogoPistonPort2);
+inline pros::adi::Pneumatics doinkerArm(doinker);
